717b8a44b98b12af0f16ebbae965948f458f76fe
Oussamayousfi1 <165747493+Oussamayousfi1@users.noreply.github.com>
2024-06-05 23:41:10 +0100

    Update index.php

--- index.php
+++ index.php
@@ -29,9 +29,9 @@ Bienvenue dans la page d'acceuil de notre application Web. Vous pouvez gérer d'
 En accédant à la liste, vous pouvez voir le détail d'un étudiant et le modifier ou le supprimer. A partir du menu, vous pouvez ajouter un nouveau étudiant ou afficher toute la liste. Testez!!
 <br />
 <hr />
-<a href ="index.html">Acceuil</a> |
-<a href ="liste.html">Liste de étudiants</a> |
-<a href ="form.html">Ajouter un étudiant</a>
+<a href ="index.php">Acceuil</a> |
+<a href ="liste.php">Liste de étudiants</a> |
+<a href ="form.php">Ajouter un étudiant</a>
 <br /><hr /><br />
 <div class="bas">&copy; copyright: D3SI 2024<br />Facult&eacute; Polydisciplinaire B&eacute;ni Mellal </br>d3si@usms.ma</div>
 </body>
